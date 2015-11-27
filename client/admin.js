488bdfc861c254077fdf6e704a46bba3a5fba552
Christiaan Hees <mail@chees.info>
2015-11-27 14:29:59 +0100

    Empty voice messages on startup

--- client/admin.js
+++ client/admin.js
@@ -1,4 +1,10 @@
 Template.admin.events({
+  'click .alarm': function(e, tmpl) {
+    console.log('alarm');
+  },
+  'click .static': function(e, tmpl) {
+    console.log('static');
+  },
   'submit form': function (e, tmpl) {
     e.preventDefault();
     var msg = e.target.querySelector('input').value;
