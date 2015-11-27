212ddf10df687a7821f27aa08189ba09587491b3
Christiaan Hees <mail@chees.info>
2015-11-27 17:55:33 +0100

    Intro voices

--- client/admin.js
+++ client/admin.js
@@ -14,6 +14,12 @@ Template.admin.helpers({
 });
 
 Template.admin.events({
+  'click [data-role="intro1"]': function(e, tmpl) {
+    VoiceMsgs.insert({msg: 'Sally Starlet here from Titan Inc. inviting you to join us on an intergalactic space flight to Via Muerta. Our luxury cruise offers comfort and entertainment throughout the entire two lightyears. As always we guarantee maximum safety from the terrors of space.', ts: new Date()});
+  },
+  'click [data-role="intro2"]': function(e, tmpl) {
+    VoiceMsgs.insert({msg: 'So sign up today and be among the first to experience the stars.', ts: new Date()});
+  },
   'click [data-role="alarm"]': function(e, tmpl) {
     State.update(1, {$set: {state: 'ALARM'}});
   },
