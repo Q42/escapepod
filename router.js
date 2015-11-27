e45e88a7e6ad5a2031cd8e1a1c244c954202ebf7
Christiaan Hees <mail@chees.info>
2015-11-27 10:35:51 +0100

    Added Iron Router and screen views

---
+++ router.js
@@ -0,0 +1,16 @@
+Router.route('/', function () {
+  this.render('Main');
+});
+
+Router.route('/s1', function () {
+  this.render('Screen1');
+});
+Router.route('/s2', function () {
+  this.render('Screen2');
+});
+Router.route('/s3', function () {
+  this.render('Screen3');
+});
+Router.route('/s4', function () {
+  this.render('Screen4');
+});
