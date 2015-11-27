93db572e6196d6c461cb12307cbd622e8e29ff1b
Christiaan Hees <mail@chees.info>
2015-11-27 11:55:13 +0100

    Voice yay

--- client/admin.js
+++ client/admin.js
@@ -2,20 +2,16 @@ Template.admin.events({
   'submit form': function (e, tmpl) {
     e.preventDefault();
     console.log('hi');
+
+    var u = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    u.text = 'I\'m afraid I can\'t do that.   Dave';
+
+    u.lang = 'en-US';
+    u.rate = 0.8;
+    u.pitch = 1.2;
+    u.volume= 0.5;
+    u.voice = voices.filter(function(voice) { return voice.name == 'Fred'; })[0];
+    speechSynthesis.speak(u);
   }
 });
-
-
-/*
-
- var u = new SpeechSynthesisUtterance();
- var voices = window.speechSynthesis.getVoices();
- u.text = 'I\'m afraid I can\'t do that.   Dave';
-
- u.lang = 'en-US';
- u.rate = 0.8;
- u.pitch = 1.2;
- u.volume= 0.5;
- u.voice = voices.filter(function(voice) { return voice.name == 'Fred'; })[0];
- speechSynthesis.speak(u);
-*/
