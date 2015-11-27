e5e8c65894b91b4ad49554f1366ccfe19eb03682
Christiaan Hees <mail@chees.info>
2015-11-27 11:32:52 +0100

    Added admin screen and static noise

--- router.js
+++ router.js
@@ -1,16 +1,20 @@
 Router.route('/', function () {
-  this.render('Main');
+  this.render('main');
 });
 
 Router.route('/s1', function () {
-  this.render('Screen1');
+  this.render('s1');
 });
 Router.route('/s2', function () {
-  this.render('Screen2');
+  this.render('s2');
 });
 Router.route('/s3', function () {
-  this.render('Screen3');
+  this.render('s3');
 });
 Router.route('/s4', function () {
-  this.render('Screen4');
+  this.render('s4');
+});
+
+Router.route('/admin', function() {
+  this.render('admin');
 });
