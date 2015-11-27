b6cb861ddfd98aaa8f39174537f03bb0ec9de6af
Christiaan Hees <mail@chees.info>
2015-11-27 11:50:12 +0100

    Fixed compile errors

--- client/admin.js
+++ client/admin.js
@@ -4,3 +4,18 @@ Template.admin.events({
     console.log('hi');
   }
 });
+
+
+/*
+
+ var u = new SpeechSynthesisUtterance();
+ var voices = window.speechSynthesis.getVoices();
+ u.text = 'I\'m afraid I can\'t do that.   Dave';
+
+ u.lang = 'en-US';
+ u.rate = 0.8;
+ u.pitch = 1.2;
+ u.volume= 0.5;
+ u.voice = voices.filter(function(voice) { return voice.name == 'Fred'; })[0];
+ speechSynthesis.speak(u);
+*/
