4aea61225159d0dd7b9edd97f0cde12f3d5b79de
Christiaan Hees <mail@chees.info>
2015-11-27 16:07:21 +0100

    Puzzel 3 state

--- client/admin.js
+++ client/admin.js
@@ -20,6 +20,9 @@ Template.admin.events({
   'click .static': function(e, tmpl) {
     State.update(1, {$set: {state: 'STATIC'}});
   },
+  'click .puzzel3': function(e, tmpl) {
+    State.update(1, {$set: {state: 'PUZZEL3'}});
+  },
   'submit form': function (e, tmpl) {
     e.preventDefault();
     var msg = e.target.querySelector('input').value;
